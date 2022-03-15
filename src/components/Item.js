b1c2117f34eb2c60d6a41aa0ca4513b88ae1582d
Pouya Shekari <pshekari37@gmail.com>
2022-03-15 11:49:19 +0330

    create snake game

---
+++ src/components/Item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Item.css';
+const Item = (props) => {
+    return (
+        <div className={props.className}>{props.children}</div>
+    );
+};
+
+export default Item;
+
+
